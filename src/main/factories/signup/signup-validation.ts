99a59a2a445165d379d346204d2a791147de6724
Marcos1305 <marcos1305pt@gmail.com>
2020-06-21 17:47:46 -0300

    refactor: group validation imports to a single file

--- src/main/factories/signup/signup-validation.ts
+++ src/main/factories/signup/signup-validation.ts
@@ -1,8 +1,10 @@
-import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
-import { RequireFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
+import {
+  ValidationComposite,
+  EmailValidation,
+  RequireFieldValidation,
+  CompareFieldsValidation
+} from '../../../presentation/helpers/validators'
 import { Validation } from '../../../presentation/protocols/validation'
-import { CompareFieldsValidation } from '../../../presentation/helpers/validators/compare-fields-validation'
-import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
 import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
 
 export const makeSignUpValidation = (): ValidationComposite => {
@@ -12,7 +14,9 @@ export const makeSignUpValidation = (): ValidationComposite => {
     validations.push(new RequireFieldValidation(field))
   }
 
-  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
+  validations.push(
+    new CompareFieldsValidation('password', 'passwordConfirmation')
+  )
   validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
 
   const validationComposite = new ValidationComposite(validations)

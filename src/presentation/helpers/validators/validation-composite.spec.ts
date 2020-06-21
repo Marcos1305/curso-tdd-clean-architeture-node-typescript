de633242d5607037f3c25bbb355e59f0fa100208
Marcos1305 <marcos1305pt@gmail.com>
2020-06-21 17:11:45 -0300

    test: ensure ValidationComposite not return if validation succeeds

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -53,4 +53,14 @@ describe('ValidationComposite', () => {
 
     expect(error).toEqual(new Error())
   })
+
+  test('Should not return if validation succeeds', () => {
+    const { sut } = makeSut()
+
+    const error = sut.validate({
+      field: 'any_value'
+    })
+
+    expect(error).toBeFalsy()
+  })
 })

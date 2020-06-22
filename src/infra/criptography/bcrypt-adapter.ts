7c12ac30495ed2bcb15b8384aec3f8c29ec29fb1
Marcos1305 <marcos1305pt@gmail.com>
2020-06-21 22:43:25 -0300

    feat: ensure DbAuthentication calls HashComparer with correct values

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -7,7 +7,7 @@ export class BcryptAdapter implements Encrypter {
     this.salt = salt
   }
 
-  async encrypt (value: String): Promise<string> {
+  async encrypt (value: string): Promise<string> {
     const hash = await bcrypt.hash(value, this.salt)
     return hash
   }
